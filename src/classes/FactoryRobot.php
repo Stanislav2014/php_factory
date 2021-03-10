34a816567b53630c3edf1ee71e12bd349a5eb347
Stanislav <s_admin@mail.ru>
2021-03-10 12:21:56 +0300

    Add Exception

--- src/classes/FactoryRobot.php
+++ src/classes/FactoryRobot.php
@@ -3,17 +3,23 @@
 class FactoryRobot
 {
     protected $types = []; // доступ только внутри класса и наследников
+    protected $methods = [];
 
     public function addType(Robot $robot)
     {
-        $this->types[get_class($robot)] = $robot;
+        $className = get_class($robot);
+        $this->types[$className] = $robot;
+        $this->methods["create{$className}"] = $className;
         return $this;
     }
 
     public function __call($method, $args)
     {
         $result = [];
-        $className = str_replace('create', '', $method);
+        if (!isset($this->methods[$method])) {
+            throw new Exception('Метод не найден');
+        }
+        $className = $this->methods[$method];
         if (isset($this->types[$className])) {
             if (!empty($args)) {
                 $countRobots = $args[0];
